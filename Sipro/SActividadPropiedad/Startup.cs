88db59ae1889205f4e9035221d2e50027d8e6d11
Enner Escobedo <eriec.42@gmail.com>
2018-09-11 15:03:59 -0600

    Inicio de angular para actividad tipo

--- Sipro/SActividadPropiedad/Startup.cs
+++ Sipro/SActividadPropiedad/Startup.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Dapper;
+using Identity;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SiproModelCore.Models;
+using Utilities;
 
 namespace SActividadPropiedad
 {
@@ -16,6 +25,19 @@ namespace SActividadPropiedad
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            var mapper = (SqlMapper.ITypeMap)Activator
+               .CreateInstance(typeof(ColumnAttributeTypeMapper<>)
+               .MakeGenericType(typeof(ActividadPropiedad)));
+            SqlMapper.SetTypeMap(typeof(ActividadPropiedad), mapper);
+            var mapper2 = (SqlMapper.ITypeMap)Activator
+               .CreateInstance(typeof(ColumnAttributeTypeMapper<>)
+               .MakeGenericType(typeof(DatoTipo)));
+            SqlMapper.SetTypeMap(typeof(DatoTipo), mapper2);
+            var mapper3 = (SqlMapper.ITypeMap)Activator
+               .CreateInstance(typeof(ColumnAttributeTypeMapper<>)
+               .MakeGenericType(typeof(ActividadPropiedadValor)));
+            SqlMapper.SetTypeMap(typeof(ActividadPropiedadValor), mapper3);
         }
 
         public IConfiguration Configuration { get; }
@@ -23,6 +45,83 @@ namespace SActividadPropiedad
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<User, Rol>()
+                .AddRoleStore<RoleStore>()
+                .AddUserStore<UserPasswordStore>()
+                .AddUserManager<CustomUserManager>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<User>, ApplicationClaimsIdentityFactory>();
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultAuthenticateScheme = "Identity.Application";
+                sharedOptions.DefaultSignInScheme = "Identity.Application";
+                // sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            });
+
+            services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo(@"/SIPRO"))
+                    .SetApplicationName("SiproApp")
+                    .DisableAutomaticKeyGeneration();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = ".AspNet.Identity.Application";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.Path = "/";
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    if (context.Response.StatusCode == (int)HttpStatusCode.OK)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    else
+                    {
+                        context.Response.Redirect(context.RedirectUri);
+                    }
+                    return Task.CompletedTask;
+                };
+
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    if (context.Response.StatusCode == (int)HttpStatusCode.OK)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    else
+                    {
+                        context.Response.Redirect(context.RedirectUri);
+                    }
+                    return Task.CompletedTask;
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Actividad Propiedades - Visualizar",
+                                  policy => policy.RequireClaim("sipro/permission", "Actividad Propiedades - Visualizar"));
+                options.AddPolicy("Actividad Propiedades - Editar",
+                                  policy => policy.RequireClaim("sipro/permission", "Actividad Propiedades - Editar"));
+                options.AddPolicy("Actividad Propiedades - Eliminar",
+                                  policy => policy.RequireClaim("sipro/permission", "Actividad Propiedades - Eliminar"));
+                options.AddPolicy("Actividad Propiedades - Crear",
+                                  policy => policy.RequireClaim("sipro/permission", "Actividad Propiedades - Crear"));
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowCredentials()
+                                 .AllowAnyMethod();
+                      });
+            });
+
             services.AddMvc();
         }
 
@@ -34,6 +133,8 @@ namespace SActividadPropiedad
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
+            app.UseCors("AllowAllHeaders");
             app.UseMvc();
         }
     }
--- Sipro/Sipro/ClientApp/src/app/components/actividadpropiedad/actividadpropiedad.component.ts
+++ Sipro/Sipro/ClientApp/src/app/components/actividadpropiedad/actividadpropiedad.component.ts
@@ -6,6 +6,7 @@ import { LocalDataSource } from 'ng2-smart-table';
 import * as moment from 'moment';
 import { MatDialog } from '@angular/material';
 import { ActividadPropiedad } from './model/ActividadPropiedad';
+import { DialogDelete, DialogOverviewDelete } from '../../../assets/modals/deleteconfirmation/confirmation-delete';
 
 @Component({
   selector: 'app-actividadpropiedad',
@@ -13,10 +14,158 @@ import { ActividadPropiedad } from './model/ActividadPropiedad';
   styleUrls: ['./actividadpropiedad.component.css']
 })
 export class ActividadpropiedadComponent implements OnInit {
+    mostrarcargando: boolean;
+    color = 'primary';
+    mode = 'indeterminate';
+    value = 50;
+    diameter = 45;
+    strokewidth = 3;
+    isLoggedIn: boolean;
+    isMasterPage: boolean;
+    esColapsado: boolean;
+    elementosPorPagina: number;
+    numeroMaximoPaginas: number;
+    totalActividadPropiedades: number;
+    actividadpropiedad: ActividadPropiedad;
+    busquedaGlobal: string;
+    paginaActual: number;
+    source: LocalDataSource;
+    esNuevo: boolean;
+    datoTipoSelected: number;
+    tipodatos = [];
+    modalDelete: DialogOverviewDelete;
 
-  constructor() { }
+    @ViewChild('search') divSearch: ElementRef;
+
+  constructor(private auth: AuthService, private utils: UtilsService, private http: HttpClient, private dialog: MatDialog) {
+    this.isMasterPage = this.auth.isLoggedIn();
+    this.utils.setIsMasterPage(this.isMasterPage);
+    this.elementosPorPagina = utils._elementosPorPagina;
+    this.numeroMaximoPaginas = utils._numeroMaximoPaginas;
+    this.totalActividadPropiedades = 0;
+    this.actividadpropiedad = new ActividadPropiedad();
+    this.source = new LocalDataSource();
+    this.modalDelete = new DialogOverviewDelete(dialog);
+   }
 
   ngOnInit() {
+    this.mostrarcargando = true;
+    this.obtenerTotalActividadPropiedades();
+    this.obtenerDatosTipo();
+  }
+
+  obtenerTotalActividadPropiedades(){
+    var data = {
+      filtro_busqueda: this.busquedaGlobal,
+      t: moment().unix()
+    };
+
+    this.http.post(
+      'http://localhost:60002/api/ActividadPropiedad/NumeroActividadPropiedades', data, { withCredentials : true })
+      .subscribe(response => {
+        if (response['success'] === true) {
+          this.totalActividadPropiedades = response['totalactividadpropiedades'];
+          this.paginaActual = 1;
+          if (this.totalActividadPropiedades > 0) {
+            this.cargarTabla(this.paginaActual);
+          } else {
+            this.source = new LocalDataSource();
+            this.mostrarcargando = false;
+          }
+        } else {
+          this.source = new LocalDataSource();
+        }
+    });
+  }
+
+  cargarTabla(pagina?: any) {
+    this.mostrarcargando = true;
+    var filtro = {
+      pagina: pagina,
+      numeroActividadPropiedades: this.elementosPorPagina,
+      filtro_busqueda: this.busquedaGlobal,
+      columna_ordenada: null,
+      t: moment().unix()
+    };
+
+    this.http.post(
+      'http://localhost:60002/api/ActividadPropiedad/ActividadPropiedadPagina',
+      filtro,
+      { withCredentials : true })
+      .subscribe(response => {
+      if (response['success'] === true) {
+        const data = response['actividadpropiedades'];
+        this.source = new LocalDataSource(data);
+        this.source.setSort([
+          { field: 'id', direction: 'asc' }  // primary sort
+        ]);
+        this.busquedaGlobal = null;
+        this.mostrarcargando = false;
+      }
+    });
+  }
+
+  obtenerDatosTipo() {
+    this.http.get('http://localhost:60017/api/DatoTipo/Listar', { withCredentials : true }).subscribe(response => {
+      if (response['success'] === true) {
+        this.tipodatos = response['datoTipos'];
+      }
+    });
+  }
+
+  cambioOpcionDatoTipo(opcion) {
+    this.actividadpropiedad.datoTipoid = opcion;
+  }
+
+  handlePage(event){
+    this.cargarTabla(event.pageIndex+1);
+  }
+
+  nuevo(){
+    this.esColapsado = true;
+    this.esNuevo = true;
+    this.componentepropiedad = new ComponentePropiedad();
+    this.datoTipoSelected = 0;
+  }
+
+  editar(){
+    if(this.componentepropiedad.id > 0){
+      this.esColapsado = true;
+      this.esNuevo = false;
+      this.datoTipoSelected = this.componentepropiedad.datoTipoid;
+    }
+    else{
+      this.utils.mensaje('warning', 'Debe seleccionar la Propiedad de componente que desea editar');
+    }
+  }
+
+  borrar(){
+    if(this.componentepropiedad.id > 0){
+      this.modalDelete.dialog.open(DialogDelete, {
+        width: '600px',
+        height: '200px',
+        data: { 
+          id: this.componentepropiedad.id,
+          titulo: 'Confirmación de Borrado', 
+          textoCuerpo: '¿Desea borrar la propiedad de componente?',
+          textoBotonOk: 'Borrar',
+          textoBotonCancelar: 'Cancelar'
+        }
+      }).afterClosed().subscribe(result => {
+        if(result != null){
+          if(result){
+            this.http.delete('http://localhost:60013/api/ComponentePropiedad/ComponentePropiedad/'+ this.componentepropiedad.id, { withCredentials : true }).subscribe(response =>{
+              if(response['success'] == true){
+                this.obtenerTotalComponentePropiedades();
+              }
+            })
+          } 
+        }
+      })
+    }
+    else{
+      this.utils.mensaje('warning', 'Seleccione una propiedad de componente');
+    }
   }
 
 }
